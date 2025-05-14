05738d2b6d7efd359035de98dce93e76f23ccd46
Kirimson <kierangates@outlook.com>
2025-05-14 10:28:44 +0100

    fix: remove echo to bashrc

--- install.sh
+++ install.sh
@@ -46,7 +46,6 @@ sudo chmod +x /usr/local/bin/sops
 curl -LO https://go.dev/dl/go1.24.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.24.3.linux-amd64.tar.gz
 rm go1.24.3.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 
 # Install Commitizen
 sudo apt update
